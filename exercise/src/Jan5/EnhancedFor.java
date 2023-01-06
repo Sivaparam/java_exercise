99aae7fdb1ed5a4c9328b458685cc23a824c32f9
ranjani <shiva.ranjani83@gmail.com>
2023-01-06 00:24:50 -0500

    Enhanced for loop example

---
+++ exercise/src/Jan5/EnhancedFor.java
@@ -0,0 +1,13 @@
+package Jan5;
+
+public class EnhancedFor {
+	
+	//Enhanced For loop example
+	public static void main(String[] args) {
+		int[] arr =  {1,2,3,4,5,6,7,8,9,10};
+		for(int i : arr) {
+			System.out.println("Array Value "  + i);
+		}
+	}
+
+}
